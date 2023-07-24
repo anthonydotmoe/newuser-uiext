946f7501a5c920fcb380295afa497f44efe840ff
Anthony Guerrero <anthony@anthony.moe>
2023-07-24 10:34:52 -0700

    fix parameter size issue

--- src/interface/idsadminnewobj.rs
+++ src/interface/idsadminnewobj.rs
@@ -2,6 +2,6 @@ use intercom::{ IUnknown, prelude::* };
 
 #[com_interface(com_iid = "F2573587-E6FC-11D2-82AF-00C04F68928B")]
 pub trait IDsAdminNewObj: IUnknown {
-    fn set_buttons(&self, curr_index: u32, valid: bool) -> ComResult<()>;
+    fn set_buttons(&self, curr_index: u32, valid: i32) -> ComResult<()>;
     fn get_page_counts(&self) -> ComResult<(u32, u32)>;
 }
